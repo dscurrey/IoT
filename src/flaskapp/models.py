1b073d1843d7eeae4ae6348ff151c3973a6dd091
Dylan Currey <9024684+dscurrey@users.noreply.github.com>
2020-05-18 15:19:47 +0100

    Added sensor values to model

--- src/flaskapp/models.py
+++ src/flaskapp/models.py
@@ -2,7 +2,11 @@ from flaskapp import db
 
 class SensorData(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    temperature = db.Column(db.Integer)
+    ir_temp = db.Column(db.Float)
+    humidity = db.Column(db.Float)
+    baro_temp = db.Column(db.Float)
+    baro_pressure = db.Column(db.Float)
+    light = db.Column(db.Float)
 
     def __repr__(self):
         return '<SensorData {}>'.format(self.temperature)
