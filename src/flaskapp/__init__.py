18e17ead0b275b5b0799ad06b652d4d7444cde2f
Dylan Currey <9024684+dscurrey@users.noreply.github.com>
2020-05-18 19:26:34 +0100

    Added bootstrap

--- src/flaskapp/__init__.py
+++ src/flaskapp/__init__.py
@@ -2,10 +2,13 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-from flaskapp import routes, models
+from flaskapp import routes, models, errors
+
+bootstrap = Bootstrap(app)
