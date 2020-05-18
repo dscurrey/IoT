a3903bc384c3a13d1ef1bf1cb87e113268601f85
Dylan Currey <9024684+dscurrey@users.noreply.github.com>
2020-05-18 13:40:18 +0100

    Created flask app

---
+++ src/flaskapp/flaskapp.py
@@ -0,0 +1,11 @@
+from flask import Flask
+import requests
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "<h1>Hello World.</h1>"
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0',port='80')
