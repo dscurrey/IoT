c8b1cb5a9d8d8edba2c2fd2afe191e219ea47719
Dylan Currey <9024684+dscurrey@users.noreply.github.com>
2020-05-18 15:05:05 +0100

    Added POST

--- src/flaskapp/routes.py
+++ src/flaskapp/routes.py
@@ -1,7 +1,19 @@
-from flaskapp import app
-from flask import render_template
+from flaskapp import app, db
+from flaskapp.models import SensorData
+from flask import render_template, request, jsonify
 
 @app.route("/")
 @app.route("/index")
 def index():
     return render_template('index.html', title='Home')
+
+@app.route('/', methods=["POST"])
+def newdata():
+    temp = request.json['temp']
+    sensordata = SensorData(temperature=temp)
+    
+    db.session.add(sensordata)
+    db.session.commit()
+
+    return jsonify(), 201
+
