f361e15a3075ddd6cc0e18b8e8ceae03f86e19bc
Stedos <29103029+Stedoss@users.noreply.github.com>
2020-05-22 02:02:48 +0100

    Start danger_calculation.

--- src/flaskapp/dangercalc.py
+++ src/flaskapp/dangercalc.py
@@ -1,2 +1,13 @@
-def dangercalc(baro_pressure,light,humidity,avg_temp):
-    pass
+from flaskapp.models import DangerLevel
+
+Critical_Temperature = 105
+Critical_Light = 40
+Critical_Humididty = 80
+
+def danger_calculation(baro_pressure,light,humidity,avg_temp):
+    if avg_temp >= Critical_Temperature:
+        return DangerLevel.Critical
+    if light >= Critical_Light:
+        return DangerLevel.Critical
+    if humidity >= Critical_Humididty:
+        return DangerLevel.Critical
